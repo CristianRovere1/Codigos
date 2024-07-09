210d2d75f05a276b022c63fb3cb3c13301f5f743
CristianRovere1 <166552450+CristianRovere1@users.noreply.github.com>
2024-07-09 14:26:44 -0300

    Delete Untitled-4.py

--- Untitled-4.py
+++
@@ -1,6 +0,0 @@
-  alturaret = float(input('Digite a altura do retângulo: '))
-baseret = float(input('Digite a base do retângulo: '))
-arearet = (alturaret*baseret)
-print('A área do retângulo é de ', arearet)
-
-   
